52a6bb96699f42ce340faa48f7fd89c3436f8b10
Davidxl <davidxl@hotmail.com>
2019-01-19 15:50:00 -0500

    add fonts and routes

---
+++ src/routes.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import Market from './components/Market';
+import Login from './components/Login';
+import Home from './components/Home';
+import AuthService from './services/auth.service'
+import './styles/index.scss';
+
+const authInstance = new AuthService();
+
+const Routes = () => (
+  <Router>
+    <div>
+      <Route exact path="/login" component={Login} auth={authInstance}/>
+      <Route exact path="/dashboard" render={() => (
+        authInstance.isLoggedIn() ? (
+          <Market auth={authInstance} />
+        ) : (
+          <Redirect to="/login"/>
+        )
+      )}/>
+      <Route exact path="/" component={Home}/>
+    </div>
+  </Router>
+);
+
+export default Routes;
