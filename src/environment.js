5eb23bee52df580c6a9f293ad01a80e8425987ea
Davidxl <davidxl@hotmail.com>
2019-01-20 13:51:00 -0500

    remove localhost

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 const config = {
   SERVER_BASE_ENDPOINT: 'https://main-coin-control.herokuapp.com',
 }
-config.SERVER_BASE_ENDPOINT = 'http://localhost:5000';
+// config.SERVER_BASE_ENDPOINT = 'http://localhost:5000';
 
 export default config;
