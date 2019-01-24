1f18b4fcca14ab87d2f465b5a69fbf4bf5e8f7dc
Davidxl <davidxl@hotmail.com>
2019-01-23 20:24:00 -0500

    fucking heroku >=(

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ const config = {
   SERVER_BASE_ENDPOINT: 'https://main-coin-control.herokuapp.com',
 }
 if (process.env.NODE_ENV === 'development') {
-  config.SERVER_BASE_ENDPOINT = 'http://localhost:5000';
+  // config.SERVER_BASE_ENDPOINT = 'http://localhost:5000';
 }
 
 export default config;
